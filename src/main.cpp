0f2808b98cae1136427a6a20ccd93b9ba6a9c091
YashKulkarni9 <awesomedude180@outlook.com>
2019-09-29 12:41:21 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,60 +16,78 @@ vex::motor MotorFrontLeft = vex::motor(vex::PORT1);
 vex::motor MotorFrontRight = vex::motor(vex::PORT2);
 vex::motor MotorBackLeft = vex::motor(vex::PORT3);
 vex::motor MotorBackRight = vex::motor(vex::PORT4);
-//Global variables here
-int axis1Ratio = ((Controller.Axis1.value()/127)*100);
-int axis2Ratio = ((Controller.Axis2.value()/127)*100);
-int axis3Ratio = ((Controller.Axis3.value()/127)*100);
-int axis4Ratio = ((Controller.Axis4.value()/127)*100);
+
+//Global variables and methods here
+float clamp(float n, float lower, float upper) {
+  return std::max(lower, std::min(n, upper));
+}
+int axis2Value = static_cast<int>(clamp(Controller.Axis2.value(), -127, 127));
+int axis1Value = static_cast<int>(clamp(Controller.Axis1.value(), -127, 127));
+int axis1Ratio = ((axis1Value/127)*100);
+int axis2Ratio = ((axis2Value/127)*100);
 
 
 
-// All real code here
+void mecanum() {
+  //up down side side
+  MotorFrontLeft.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+  MotorFrontRight.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+  MotorBackLeft.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+  MotorBackRight.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+  MotorFrontLeft.spin(vex::directionType::fwd,axis1Ratio,velocityUnits::pct);
+  MotorFrontRight.spin(vex::directionType::fwd,-axis1Ratio,velocityUnits::pct);
+  MotorBackLeft.spin(vex::directionType::fwd,-axis1Ratio,velocityUnits::pct);
+  MotorBackRight.spin(vex::directionType::fwd,axis1Ratio,velocityUnits::pct);
+  //diagonal movements
+  if ((axis2Value > 0 && axis1Value > 0) || (axis2Value < 0 && axis1Value < 0)) {
+    MotorFrontLeft.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+    MotorFrontRight.spin(vex::directionType::fwd,0,velocityUnits::pct);
+    MotorBackLeft.spin(vex::directionType::fwd,0,velocityUnits::pct);
+    MotorBackRight.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+  } 
+  if ((axis2Value < 0 && axis1Value > 0) || (axis2Value > 0 && axis1Value < 0)) {
+    MotorFrontLeft.spin(vex::directionType::fwd,0,velocityUnits::pct);
+    MotorFrontRight.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+    MotorBackLeft.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
+    MotorBackRight.spin(vex::directionType::fwd,0,velocityUnits::pct);
+  }
+  if (axis2Value == 0 && axis1Value == 0) {
+    MotorFrontLeft.stop(vex::brakeType::coast);
+    MotorFrontRight.stop(vex::brakeType::coast);
+    MotorBackLeft.stop(vex::brakeType::coast);
+    MotorBackRight.stop(vex::brakeType::coast);
+  }  
+}
+
+void intake() {
+
+}
+
+void rail() {
+
+}
+
 void autonomous() {
+//3.5 rotations
 
 }
 
 void drivercontrol() {
-// mecanum drive
-
   while (true) {
-
-  axis1Ratio = ((Controller.Axis1.value()/127)*100);
-  axis2Ratio = ((Controller.Axis2.value()/127)*100);
-  axis3Ratio = ((Controller.Axis3.value()/127)*100);
-  axis4Ratio = ((Controller.Axis4.value()/127)*100);
-
-    
-      MotorFrontLeft.spin(vex::directionType::fwd,axis1Ratio,velocityUnits::pct);
-      MotorFrontRight.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
-      MotorBackLeft.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
-      MotorBackRight.spin(vex::directionType::fwd,axis2Ratio,velocityUnits::pct);
-      if (Controller.Axis1.value() == 0) {
-      MotorFrontLeft.stop();
-      MotorFrontRight.stop();
-      MotorBackLeft.stop();
-      MotorBackRight.stop();
-      }
-    
-
- 
+    mecanum(); 
+    intake();
+    rail();
   }
-
-}
-
-float clamp(float n, float lower, float upper) {
-  return std::max(lower, std::min(n, upper));
 }
 
 int main() {
   Competition.autonomous(autonomous);
   Competition.drivercontrol(drivercontrol);
-  // test things here
   while(true){
-    int Axis1N = static_cast<int>(clamp(Controller.Axis1.value(), -127, 127));
-
     Brain.Screen.clearScreen();
-    Brain.Screen.print(Axis1N);
+    Brain.Screen.print(axis2Value);
     Brain.Screen.newLine();
   }
 }
+
+
