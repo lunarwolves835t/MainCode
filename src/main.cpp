00a1f2d384d5d09824e057c0c26333715391cb7b
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2019-11-16 02:08:00 -0800

    ???

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,4 @@
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Arm                  motor         10              
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Rail                 motor         5               
-// Arm                  motor         10              
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// Rail                 motor         5               
-// Arm                  motor         10              
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// BackRight            motor         4               
-// Controller1          controller                    
-// Rail                 motor         5               
-// Arm                  motor         10              
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// BackLeft             motor         3               
-// BackRight            motor         4               
-// Controller1          controller                    
-// Rail                 motor         5               
-// Arm                  motor         10              
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// FrontLeft            motor         2               
-// BackLeft             motor         3               
-// BackRight            motor         4               
-// Controller1          controller                    
-// Rail                 motor         5               
-// Arm                  motor         10              
-// RightHand            motor         9               
-// LeftHand             motor         8               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\amayb                                            */
-/*    Created:      Sat Nov 09 2019                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
- 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// ---- END VEXCODE CONFIGURED DEVICES ----
  
 #include "vex.h"
  
@@ -101,8 +18,10 @@ vex::motor MotorRail = vex::motor(vex::PORT5);
 
 
 class Cont{
-  public:
-  void arms(void){
+  //works: arms, drivebase, rails
+  //doesn't work: hands
+  public: 
+  void arms(void){ 
         //arms
         if (Controller.ButtonDown.pressing()) {
           // MotorIntakeArm.stop(vex::brakeType::brake);
@@ -118,7 +37,7 @@ class Cont{
         }
     
   }
-  void hands(void){
+  void hands(void){ 
         if (Controller.ButtonA.pressing()) {
           MotorIntakeLeft.spin(vex::directionType::fwd,-20,velocityUnits::pct);
           MotorIntakeRight.spin(vex::directionType::fwd,20,velocityUnits::pct);
@@ -133,7 +52,7 @@ class Cont{
         }
   }
   void driveBase(void){
-    int ratio = 200;
+    int ratio = 250; //increase to make drive slower
     double speedRight = Controller.Axis1.position(percent)*70 - Controller.Axis3.position(percent)*100;
     speedRight /= ratio;
     MotorFrontRight.spin(directionType::fwd, speedRight, percent);
@@ -146,12 +65,12 @@ class Cont{
   }
   void rails(void){
     //Rail.setMaxTorque(1, percentUnits::pct);
-    MotorRail.setMaxTorque(100, percentUnits::pct);
+    MotorRail.setMaxTorque(90, percentUnits::pct);
     if(Controller.ButtonX.pressing()){
-    MotorRail.spin(directionType::fwd, 100, velocityUnits::pct);
+    MotorRail.spin(directionType::fwd, 30, velocityUnits::pct);
     }
     else if(Controller.ButtonB.pressing()){
-    MotorRail.spin(directionType::fwd, -100, velocityUnits::pct);
+    MotorRail.spin(directionType::fwd, -30, velocityUnits::pct);
     }
     else{
       MotorRail.stop(coast);
@@ -167,59 +86,114 @@ class Auton{
   void move(double dist){
     const double circum = 4 * 3.1415;
     double revo = dist/circum;
+    MotorBackLeft.setVelocity(50, velocityUnits::pct);
+    MotorBackRight.setVelocity(50, velocityUnits::pct);
+    MotorFrontLeft.setVelocity(50, velocityUnits::pct);
+    MotorFrontRight.setVelocity(50, velocityUnits::pct);
     MotorFrontLeft.rotateFor(revo, rotationUnits::rev, false);
-    MotorFrontRight.rotateFor(revo, rotationUnits::rev, false);
+    MotorFrontRight.rotateFor(-revo, rotationUnits::rev, false);
     MotorBackLeft.rotateFor(revo, rotationUnits::rev, false);
-    MotorBackRight.rotateFor(revo, rotationUnits::rev);
+    MotorBackRight.rotateFor(-revo, rotationUnits::rev);
   }
 
-  void turnn(int direc){  //direc = 1 || -1
+  void turnn(int direc){  //direc = -1 || 1
     const double vel = 50;
-    MotorFrontLeft.spin(fwd, vel, velocityUnits::pct);
+    MotorFrontLeft.spin(fwd, -vel, velocityUnits::pct);
     MotorFrontRight.spin(fwd, -vel, velocityUnits::pct);
     MotorBackRight.spin(fwd, -vel, velocityUnits::pct);
-    MotorBackLeft.spin(fwd, vel, velocityUnits::pct);
+    MotorBackLeft.spin(fwd, -vel, velocityUnits::pct);
+    task::sleep(100); //increase this to increase turn
+    MotorBackLeft.stop();
+    MotorBackRight.stop();
+    MotorFrontLeft.stop();
+    MotorFrontRight.stop();
   }
 
   void pickup(int num){
     for(int i = 0; i < num; i++){
       MotorIntakeLeft.spin(fwd, 100, velocityUnits::pct);
-      MotorIntakeRight.spin(fwd, 100, velocityUnits::pct);
+      MotorIntakeRight.spin(fwd, -100, velocityUnits::pct);
+      task::sleep(500);
       move(5.5);
+      MotorIntakeRight.stop();
+      MotorIntakeLeft.stop();
+    
+    
+    
+    
     }
   }
 
+  void dropoff(int num){
+    MotorRail.rotateFor(fwd, .5, rev);
+    for(int i = 0; i < num; i++){
+      MotorIntakeLeft.spin(fwd, -30, velocityUnits::pct);
+      MotorIntakeRight.spin(fwd, 30, velocityUnits::pct);
+      task::sleep(500);
+      MotorIntakeLeft.stop();
+      MotorIntakeRight.stop();
+      MotorRail.rotateFor(fwd, -.5, rev);
+    }
+  }
+
+
   void instructions(void){
-    move(23.5);
+    move(23.5); //23.5
     pickup(4);
     turnn(-1);
-    move(12);
-    turnn(-1);
+    // move(12);
+    // turnn(-1);
 
   }
 };
 
+void autonomous(void){
+  Auton auton;
+  MotorFrontRight.setVelocity(50, percentUnits::pct);
+  MotorFrontLeft.setVelocity(50, percentUnits::pct);
+  MotorBackRight.setVelocity(50, percentUnits::pct);
+  MotorBackLeft.setVelocity(50, percentUnits::pct);
+  auton.instructions();
+}
+
+void usercontrol(void){
+  Cont cont;
+  while(true){
+    cont.hands();
+    cont.driveBase();
+    cont.rails();
+    cont.arms();
+    vex::task::sleep(20);
+  }
+}
+
+void pre_auton(){
+  MotorIntakeArm.setMaxTorque(100, pct);
+  MotorIntakeArm.setVelocity(100, pct);
+    MotorIntakeArm.rotateFor(2, rotationUnits::rev);
+    
+    MotorIntakeArm.rotateFor(-1.75, rotationUnits::rev);
+    MotorIntakeArm.rotateFor(2, rotationUnits::rev);
+    
+    MotorIntakeArm.rotateFor(-1.75, rotationUnits::rev);
+}
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  bool controll = false;
-  Cont cont;
-  Auton auton;
-  if(controll){
-    while (true) {
-      cont.hands();
-      cont.driveBase();
-      cont.rails();
-      cont.arms();
-    }
-  }
-  else{
-    while(true){
-      MotorFrontRight.setVelocity(50, percentUnits::pct);
-      MotorFrontLeft.setVelocity(50, percentUnits::pct);
-      MotorBackRight.setVelocity(50, percentUnits::pct);
-      MotorBackLeft.setVelocity(50, percentUnits::pct);
-      auton.instructions();
-    }
+
+  pre_auton();
+  // tell the competition code what to run for the autonomous period
+  // while(true){
+  //   usercontrol();
+  // }
+  // autonomous();
+  Competition.autonomous( autonomous );
+  // tell the competition code what to run for the driver control period
+  Competition.drivercontrol( usercontrol );
+  
+  //Prevent main from exiting with an infinite loop.
+  while(1) {
+      task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
   }
 }
